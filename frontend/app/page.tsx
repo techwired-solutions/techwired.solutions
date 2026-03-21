c92be5ebbffcdb2ef364c42d06485d3b5e24c9f9
techwired-solutions <s.techwired@gmail.com>
2026-03-21 17:35:57 +0545

    Update page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
       <HeroSection />
       <AboutSection />
       <FeaturesSection />
-      <ServicesSection />
+      {/* <ServicesSection /> */}
       {/* <ProjectsSection /> */}
       <PartnersSection /> 
       <ContactSection />
